09780e3b971b315cb09457e28d6074535fec210c
shumi-debnath <70173844+shumi-debnath@users.noreply.github.com>
2020-11-19 01:51:05 -0500

    Add files via upload

---
+++ OneDirectionCollection/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	const videos = document.querySelectorAll("video")
+
+	videos.forEach(video => {
+		video.addEventListener("mouseover", function() {
+			this.play()
+		})
+
+		video.addEventListener("mouseout", function(){
+			this.pause()
+		})
+	})
+});
